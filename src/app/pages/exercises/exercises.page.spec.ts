3a5b5261d974d232ebc7c5cb030d44df93cc08d2
Ken Sodemann <ken@ionic.io>
2019-06-16 16:10:26 -0500

    feat(exercises): get the list of exercises

--- src/app/pages/exercises/exercises.page.spec.ts
+++ src/app/pages/exercises/exercises.page.spec.ts
@@ -3,28 +3,47 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { ModalController } from '@ionic/angular';
 
 import { ExercisesPage } from './exercises.page';
-import { AuthenticationService } from '../../services/authentication/authentication.service';
+import { AuthenticationService } from 'src/app/services/authentication/authentication.service';
 import { ExerciseEditorComponent } from 'src/app/editors/exercise-editor/exercise-editor.component';
+import { ExercisesService } from 'src/app/services/firestore-data/exercises/exercises.service';
 
-import { createAuthenticationServiceMock } from '../../services/authentication/authentication.service.mock';
+import { createAuthenticationServiceMock } from 'src/app/services/authentication/authentication.service.mock';
+import { createExercisesServiceMock } from 'src/app/services/firestore-data/exercises/exercises.service.mock';
 import { createOverlayControllerMock, createOverlayElementMock } from 'test/mocks';
+import { Exercise } from 'src/app/models/exercise';
+import { Subject } from 'rxjs';
+import { ExerciseFocusAreas } from 'src/app/default-data';
 
 describe('ExercisesPage', () => {
   let component: ExercisesPage;
   let editor;
+  let exercisesList: Subject<Array<Exercise>>;
+  let exercises: Array<Exercise>;
+  let sortedExercises: Array<{ area: string; exercises: Array<Exercise> }>;
   let fixture: ComponentFixture<ExercisesPage>;
 
   beforeEach(async(() => {
+    initiailzeTestData();
     editor = createOverlayElementMock('Modal');
     TestBed.configureTestingModule({
       declarations: [ExercisesPage],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
       providers: [
         { provide: AuthenticationService, useFactory: createAuthenticationServiceMock },
-        { provide: ModalController, useFactory: () => createOverlayControllerMock('ModalController', editor) }
+        { provide: ExercisesService, useFactory: createExercisesServiceMock },
+        {
+          provide: ModalController,
+          useFactory: () => createOverlayControllerMock('ModalController', editor)
+        }
       ]
     }).compileComponents();
   }));
+
+  beforeEach(() => {
+    const svc = TestBed.get(ExercisesService);
+    exercisesList = new Subject();
+    svc.all.and.returnValue(exercisesList);
+  });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ExercisesPage);
@@ -32,8 +51,21 @@ describe('ExercisesPage', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('builds', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('initialization', () => {
+    it('sets up an observable on the exercises', () => {
+      const svc = TestBed.get(ExercisesService);
+      expect(svc.all).toHaveBeenCalledTimes(1);
+    });
+
+    it('sets the exercises to the sorted list of exercises segregated by area', () => {
+      const svc = TestBed.get(ExercisesService);
+      exercisesList.next(exercises);
+      expect(component.exercisesByArea).toEqual(sortedExercises);
+    });
   });
 
   describe('add', () => {
@@ -41,7 +73,9 @@ describe('ExercisesPage', () => {
       const modalController = TestBed.get(ModalController);
       component.add();
       expect(modalController.create).toHaveBeenCalledTimes(1);
-      expect(modalController.create).toHaveBeenCalledWith({ component: ExerciseEditorComponent });
+      expect(modalController.create).toHaveBeenCalledWith({
+        component: ExerciseEditorComponent
+      });
     });
 
     it('presents the editor', async () => {
@@ -50,4 +84,98 @@ describe('ExercisesPage', () => {
       expect(editor.present).toHaveBeenCalledTimes(1);
     });
   });
+
+  function initiailzeTestData() {
+    exercises = [
+      {
+        id: '388495883',
+        name: 'Bench Press',
+        description: 'Standard bench press with a barbell',
+        area: 'Upper Body',
+        type: 'Free Weight'
+      },
+      {
+        id: 'A98503BEF',
+        name: 'Sit-up',
+        description: 'Lay on back with legs bent, sit up fully',
+        area: 'Core',
+        type: 'Body Weight'
+      },
+      {
+        id: '9930408A3',
+        name: 'Elliptical',
+        description: 'Low impact glide-running',
+        area: 'Cardio',
+        type: 'Machine'
+      },
+      {
+        id: '3885723475',
+        name: 'Matrix Bench Press',
+        description: 'Bench press using a machine',
+        area: 'Upper Body',
+        type: 'Machine'
+      },
+      {
+        id: '773758FC3',
+        name: 'Dumbbell Bench Press',
+        description: 'Bench press using two dumbbells',
+        area: 'Upper Body',
+        type: 'Free Weight'
+      }
+    ];
+    sortedExercises = [
+      {
+        area: ExerciseFocusAreas.Cardio,
+        exercises: [
+          {
+            id: '9930408A3',
+            name: 'Elliptical',
+            description: 'Low impact glide-running',
+            area: 'Cardio',
+            type: 'Machine'
+          }
+        ]
+      },
+      {
+        area: ExerciseFocusAreas.Core,
+        exercises: [
+          {
+            id: 'A98503BEF',
+            name: 'Sit-up',
+            description: 'Lay on back with legs bent, sit up fully',
+            area: 'Core',
+            type: 'Body Weight'
+          }
+        ]
+      },
+      {
+        area: ExerciseFocusAreas.UpperBody,
+        exercises: [
+          {
+            id: '388495883',
+            name: 'Bench Press',
+            description: 'Standard bench press with a barbell',
+            area: 'Upper Body',
+            type: 'Free Weight'
+          },
+          {
+            id: '773758FC3',
+            name: 'Dumbbell Bench Press',
+            description: 'Bench press using two dumbbells',
+            area: 'Upper Body',
+            type: 'Free Weight'
+          },
+          {
+            id: '3885723475',
+            name: 'Matrix Bench Press',
+            description: 'Bench press using a machine',
+            area: 'Upper Body',
+            type: 'Machine'
+          }
+        ]
+      },
+      { area: ExerciseFocusAreas.LowerBody, exercises: [] },
+      { area: ExerciseFocusAreas.FullBody, exercises: [] }
+    ];
+  }
 });
--- src/app/pages/exercises/exercises.page.ts
+++ src/app/pages/exercises/exercises.page.ts
@@ -1,20 +1,60 @@
-import { Component, OnInit } from '@angular/core';
-import { AuthenticationService } from '../../services/authentication/authentication.service';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ModalController } from '@ionic/angular';
-import { ExerciseEditorComponent } from 'src/app/editors/exercise-editor/exercise-editor.component';
+import { Subscription } from 'rxjs';
 
+import { AuthenticationService } from 'src/app/services/authentication/authentication.service';
+import { byName } from 'src/app/util';
+import { exerciseFocusAreas } from 'src/app/default-data';
+import { Exercise } from 'src/app/models/exercise';
+import { ExerciseEditorComponent } from 'src/app/editors/exercise-editor/exercise-editor.component';
+import { ExercisesService } from 'src/app/services/firestore-data/exercises/exercises.service';
+
+interface AreaExercises {
+  area: string;
+  exercises: Array<Exercise>;
+}
 @Component({
   selector: 'app-exercises',
   templateUrl: './exercises.page.html',
   styleUrls: ['./exercises.page.scss']
 })
-export class ExercisesPage implements OnInit {
-  constructor(public authentication: AuthenticationService, private modalController: ModalController) {}
+export class ExercisesPage implements OnInit, OnDestroy {
+  private exercisesSubscription: Subscription;
 
-  ngOnInit() {}
+  exercisesByArea: Array<AreaExercises>;
+
+  constructor(
+    public authentication: AuthenticationService,
+    private exercisesService: ExercisesService,
+    private modalController: ModalController
+  ) {}
+
+  ngOnInit() {
+    this.exercisesSubscription = this.exercisesService
+      .all()
+      .subscribe(all => this.processExercises(all));
+  }
+
+  ngOnDestroy() {
+    this.exercisesSubscription.unsubscribe();
+  }
 
   async add(): Promise<void> {
     const modal = await this.modalController.create({ component: ExerciseEditorComponent });
     modal.present();
+  }
+
+  private processExercises(all: Array<Exercise>) {
+    this.exercisesByArea = [];
+    exerciseFocusAreas.forEach(area =>
+      this.exercisesByArea.push(this.exercisesForArea(all, area))
+    );
+  }
+
+  private exercisesForArea(all: Array<Exercise>, area: string): AreaExercises {
+    return {
+      area,
+      exercises: all.filter(e => e.area === area).sort(byName)
+    };
   }
 }
